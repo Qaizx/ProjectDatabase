a4e3ef2a160a9a7db19a201dc9a938f937b40434
Qaizx <kaothom@gmail.com>
2022-09-27 17:41:26 +0700

    upload front-end

---
+++ front-end/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import home from './web/home/home';
+import Navbar from './component/Navbar';
+
+function App() {
+  return (
+    <div>
+      <Navbar/>
+      <h1>Hello</h1>
+    </div>
+  );
+}
+
+export default App;
