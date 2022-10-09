ba48d755a996c27bc4c5e1e17444c8fa62f383ec
Qaizx <kaothom@gmail.com>
2022-10-10 01:41:26 +0700

    register page

---
+++ front-end/src/web/register/register.js
@@ -0,0 +1,40 @@
+import "./Register.css"
+
+const Register = () => {
+    return(
+        <div>
+        <div class="head"> Register</div>
+  
+        <div class="info">
+          <div class="text_info">
+            <label>username / email</label>
+            <input class="input_info1"></input>
+          </div>
+  
+          <div class="text_info">
+            <label>username / email</label>
+            <input class="input_info1"></input>
+          </div>
+
+          <div class="text_info">
+            <label>username / email</label>
+            <input class="input_info1"></input>
+          </div>
+
+          <div class="text_info">
+            <label>username / email</label>
+            <input class="input_info1"></input>
+          </div>
+        </div>
+  
+        <div class="All_button">
+          
+          <input type="button" class="register_button" value="register"></input>
+        </div>
+  
+        
+      </div>
+    )
+}
+
+export default Register;
