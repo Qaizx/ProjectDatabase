b0570271b6351486a89bd97ad45a653059b80a81
Qaizx <kaothom@gmail.com>
2022-10-20 02:30:49 +0700

    home page

--- front-end/src/Page/home/Home.js
+++ front-end/src/Page/home/Home.js
@@ -1,11 +1,41 @@
+import "./Home.css";
+import { Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
+
 const Home = () => {
-    return(
-        <div>
-            <h1>Hello</h1>
-
-
-        </div>
-    )
-}
+  return (
+    <>
+      <h1 class="welcomeText">
+        Welcome to <span style={{ color: "red" }}>shop</span>
+      </h1>
+      <div style={{ textAlign: "center" }}>
+        <button class="get_start">
+          <span>
+            Get Started &nbsp;
+            <svg
+              stroke="currentColor"
+              fill="currentColor"
+              stroke-width="0"
+              viewBox="0 0 448 512"
+              class="react-icon"
+              height="1em"
+              width="1em"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path d="M190.5 66.9l22.2-22.2c9.4-9.4 24.6-9.4 33.9 0L441 239c9.4 9.4 9.4 24.6 0 33.9L246.6 467.3c-9.4 9.4-24.6 9.4-33.9 0l-22.2-22.2c-9.5-9.5-9.3-25 .4-34.3L311.4 296H24c-13.3 0-24-10.7-24-24v-32c0-13.3 10.7-24 24-24h287.4L190.9 101.2c-9.8-9.3-10-24.8-.4-34.3z"></path>
+            </svg>
+          </span>
+        </button>
+        
+        <button class="product_today">
+          <a href="#down" style={{color: "white"}}>Product Today</a>
+        </button>
+      </div>
+      <div id="down">
+        <h1>You are down!</h1>
+      </div>
+    </>
+  );
+};
 
 export default Home;
