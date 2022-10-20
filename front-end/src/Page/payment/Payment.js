ef2536fbc65b1141c7eeef147a979fd3fb838414
aumrcn <aumkung20728@gmail.com>
2022-10-20 23:26:06 +0700

    payment v1

--- front-end/src/Page/payment/Payment.js
+++ front-end/src/Page/payment/Payment.js
@@ -5,7 +5,66 @@ import React from "react"
 const Payment = () => {
     return (
         <div>
-            <div class="head"> This is Payment page</div>
+            <div>
+                <h1> This is Payment page</h1>
+                <div class="head">Order History</div>
+            </div>
+            <div>
+                <div class="row px-5">
+                    <div class="card">
+                        <div class="card-body">
+                            <div class="row">
+                                <div class="col-md-1">
+                                    <img src="https://www.adameve.com/cms/image/623024-695870-350x350.jpg"
+                                        class="mr-2" width="150" height="150" alt="Product" />
+                                </div>
+                                <div class="col">
+                                    <h2 class="d-flex justify-content-center mt-5 mx-5">Product Name</h2>
+                                </div>
+                                <div class="col">
+                                    <h2 class="d-flex justify-content-center mt-5 mx-5">Quantity : 69</h2>
+                                </div>
+                                <div class="col">
+                                    <h2 class="d-flex justify-content-center mt-5 mx-5">Total : $5000</h2>
+                                </div>
+                            </div>
+                            <hr class="my-2" />
+                            <div class="row">
+                                <div class="col-md-1">
+                                    <img src="https://www.adameve.com/cms/image/623024-695870-350x350.jpg"
+                                        class="mr-2" width="150" height="150" alt="Product" />
+                                </div>
+                                <div class="col">
+                                    <h2 class="d-flex justify-content-center mt-5 mx-5">Product Name</h2>
+                                </div>
+                                <div class="col">
+                                    <h2 class="d-flex justify-content-center mt-5 mx-5">Quantity : 69</h2>
+                                </div>
+                                <div class="col">
+                                    <h2 class="d-flex justify-content-center mt-5 mx-5">Total : $5000</h2>
+                                </div>
+                            </div>
+                            <hr class="my-2" />
+                            <div class="row">
+                                <div class="col-md-1">
+                                    <img src="https://www.adameve.com/cms/image/623024-695870-350x350.jpg"
+                                        class="mr-2" width="150" height="150" alt="Product" />
+                                </div>
+                                <div class="col">
+                                    <h2 class="d-flex justify-content-center mt-5 mx-5">Product Name</h2>
+                                </div>
+                                <div class="col">
+                                    <h2 class="d-flex justify-content-center mt-5 mx-5">Quantity : 69</h2>
+                                </div>
+                                <div class="col">
+                                    <h2 class="d-flex justify-content-center mt-5 mx-5">Total : $5000</h2>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
         </div>
     )
 }
