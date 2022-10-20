9b988a7b3fa25f6f7b32b29db3bd5984b4733c62
aumrcn <aumkung20728@gmail.com>
2022-10-20 18:53:23 +0700

    cart and shop page

--- front-end/src/Page/shop/Shop.js
+++ front-end/src/Page/shop/Shop.js
@@ -1,70 +1,82 @@
 import "./Shop.css"
-import joji from "./images/joji.jfif"
 import { Button, Row, Form, Col, Container, Card } from "react-bootstrap";
 import React from "react"
 
 const Shop = () => {
     return (
         <div clas="flex-column">
-            <div class="content">
-                <h1>This is Shop page</h1>
+            <div class="">
+                <h1 class="ms-5 mt-5">This is Shop page</h1>
             </div>
-            <div class="container margin-top:5000px">
-                <div class="row align-items-start">
-                    <div class="col">
-                        <div class="card" className="cardcss">
-                            <img class="card-img-top" src="https://i.scdn.co/image/ab67616d00001e0260ba1d6104d0475c7555a6b2" alt="Card img" />
-                            <div class="card-body">
-                                <h4 class="card-title">John Doe</h4>
-                                <p class="card-text">Some example text.</p>
-                                <a href="" class="btn btn-primary">See Profile</a>
+            <div class="mt-5">
+                <div class="container ">
+                    <div class="row align-items-start mb-5">
+                        <div class="col">
+                            <div class="card" className="cardcss">
+                                <img class="card-img-top" src="https://i.scdn.co/image/ab67616d00001e0260ba1d6104d0475c7555a6b2" alt="Card img" />
+                                <div class="card-body">
+                                    <h4 class="card-title">Product Name</h4>
+                                    <p class="card-text">Description</p>
+                                    <a href="" class="btn btn-primary">Add to cart</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col">
+                            <div class="card" className="cardcss">
+                                <img class="card-img-top" src="https://i.scdn.co/image/ab67616d00001e0260ba1d6104d0475c7555a6b2" alt="Card img" />
+                                <div class="card-body">
+                                    <h4 class="card-title">Product Name</h4>
+                                    <p class="card-text">Description</p>
+                                    <a href="" class="btn btn-primary">Add to cart</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col">
+                            <div class="card" className="cardcss">
+                                <img class="card-img-top" src="https://i.scdn.co/image/ab67616d00001e0260ba1d6104d0475c7555a6b2" alt="Card img" />
+                                <div class="card-body">
+                                    <h4 class="card-title">Product Name</h4>
+                                    <p class="card-text">Description</p>
+                                    <a href="" class="btn btn-primary">Add to cart</a>
+                                </div>
                             </div>
                         </div>
                     </div>
-                    <div class="col">
-                        <div class="card" className="cardcss">
-                            <img class="card-img-top" src="https://i.scdn.co/image/ab67616d00001e0260ba1d6104d0475c7555a6b2" alt="Card img" />
-                            <div class="card-body">
-                                <h4 class="card-title">John Doe</h4>
-                                <p class="card-text">Some example text.</p>
-                                <a href="" class="btn btn-primary">See Profile</a>
+                    <div class="row align-items-start mb-5">
+                        <div class="col">
+                            <div class="card" className="cardcss">
+                                <img class="card-img-top" src="https://i.scdn.co/image/ab67616d00001e0260ba1d6104d0475c7555a6b2" alt="Card img" />
+                                <div class="card-body">
+                                    <h4 class="card-title">Product Name</h4>
+                                    <p class="card-text">Description</p>
+                                    <a href="" class="btn btn-primary">Add to cart</a>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    <div class="col">
-                        <div class="card" className="cardcss">
-                            <img class="card-img-top" src="https://i.scdn.co/image/ab67616d00001e0260ba1d6104d0475c7555a6b2" alt="Card img" />
-                            <div class="card-body">
-                                <h4 class="card-title">John Doe</h4>
-                                <p class="card-text">Some example text.</p>
-                                <a href="" class="btn btn-primary">See Profile</a>
+                        <div class="col">
+                            <div class="card" className="cardcss">
+                                <img class="card-img-top" src="https://i.scdn.co/image/ab67616d00001e0260ba1d6104d0475c7555a6b2" alt="Card img" />
+                                <div class="card-body">
+                                    <h4 class="card-title">Product Name</h4>
+                                    <p class="card-text">Description</p>
+                                    <a href="" class="btn btn-primary">Add to cart</a>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                </div>
-                <div class="row align-items-center">
-                    <div class="col">
-                        One of three columns
-                    </div>
-                    <div class="col">
-                        One of three columns
-                    </div>
-                    <div class="col">
-                        One of three columns
-                    </div>
-                </div>
-                <div class="row align-items-end">
-                    <div class="col">
-                        One of three columns
-                    </div>
-                    <div class="col">
-                        One of three columns
-                    </div>
-                    <div class="col">
-                        One of three columns
+                        <div class="col">
+                            <div class="card" className="cardcss">
+                                <img class="card-img-top" src="https://i.scdn.co/image/ab67616d00001e0260ba1d6104d0475c7555a6b2" alt="Card img" />
+                                <div class="card-body">
+                                    <h4 class="card-title">Product Name</h4>
+                                    <p class="card-text">Description</p>
+                                    <a href="" class="btn btn-primary">Add to cart</a>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
+
         </div>
     )
 }
