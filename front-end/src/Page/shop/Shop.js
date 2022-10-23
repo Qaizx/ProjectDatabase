d8a33f4b8bd4e9108527b6738205a57918bc9e0f
Qaizx <kaothom@gmail.com>
2022-10-23 16:41:56 +0700

    shop page

--- front-end/src/Page/shop/Shop.js
+++ front-end/src/Page/shop/Shop.js
@@ -1,85 +1,83 @@
-import "./Shop.css"
+import "./Shop.css";
 import { Button, Row, Form, Col, Container, Card } from "react-bootstrap";
-import React from "react"
+import React from "react";
+import { useState, useEffect } from "react";
 
 const Shop = () => {
-    return (
-        <div clas="flex-column">
-            <div class="">
-                <h1 class="ms-5 mt-5">This is Shop page</h1>
-            </div>
-            <div class="mt-5">
-                <div class="container ">
-                    <div class="row align-items-start mb-5">
-                        <div class="col">
-                            <div class="card" className="cardcss">
-                                <img class="card-img-top" src="https://i.scdn.co/image/ab67616d00001e0260ba1d6104d0475c7555a6b2" alt="Card img" />
-                                <div class="card-body">
-                                    <h4 class="card-title">Product Name</h4>
-                                    <p class="card-text">Description</p>
-                                    <a href="" class="btn btn-primary">Add to cart</a>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col">
-                            <div class="card" className="cardcss">
-                                <img class="card-img-top" src="https://i.scdn.co/image/ab67616d00001e0260ba1d6104d0475c7555a6b2" alt="Card img" />
-                                <div class="card-body">
-                                    <h4 class="card-title">Product Name</h4>
-                                    <p class="card-text">Description</p>
-                                    <a href="" class="btn btn-primary">Add to cart</a>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col">
-                            <div class="card" className="cardcss">
-                                <img class="card-img-top" src="https://i.scdn.co/image/ab67616d00001e0260ba1d6104d0475c7555a6b2" alt="Card img" />
-                                <div class="card-body">
-                                    <h4 class="card-title">Product Name</h4>
-                                    <p class="card-text">Description</p>
-                                    <a href="" class="btn btn-primary">Add to cart</a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="row align-items-start mb-5">
-                        <div class="col">
-                            <div class="card" className="cardcss">
-                                <img class="card-img-top" src="https://i.scdn.co/image/ab67616d00001e0260ba1d6104d0475c7555a6b2" alt="Card img" />
-                                <div class="card-body">
-                                    <h4 class="card-title">Product Name</h4>
-                                    <p class="card-text">Description</p>
-                                    <a href="" class="btn btn-primary">Add to cart</a>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col">
-                            <div class="card" className="cardcss">
-                                <img class="card-img-top" src="https://i.scdn.co/image/ab67616d00001e0260ba1d6104d0475c7555a6b2" alt="Card img" />
-                                <div class="card-body">
-                                    <h4 class="card-title">Product Name</h4>
-                                    <p class="card-text">Description</p>
-                                    <a href="" class="btn btn-primary">Add to cart</a>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col">
-                            <div class="card" className="cardcss">
-                                <img class="card-img-top" src="https://i.scdn.co/image/ab67616d00001e0260ba1d6104d0475c7555a6b2" alt="Card img" />
-                                <div class="card-body">
-                                    <h4 class="card-title">Product Name</h4>
-                                    <p class="card-text">Description</p>
-                                    <a href="" class="btn btn-primary">Add to cart</a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+  const [products, setProducts] = useState([]);
+  const [check, setChecked] = useState(true);
 
+  const initProducts = async () => {
+    var myHeaders = new Headers();
+    myHeaders.append(
+      "Cookie",
+      "XSRF-TOKEN=eyJpdiI6IldZOE12U1d5WVpqVkg4WUdoVDdZUlE9PSIsInZhbHVlIjoiQUIxNndzdzh5UkwwYVB5RFNZVWkvR1Axa1BkYW5zRXM1bE1hVjNUZHRjNlFNU2xRd3pNSWZOS25JZEwwWW4zZHU2bDlybGdmSFMwUnFtalNhNmxnUXlTNEdqbkNmdzJ4L2FKNFBvb1dSdlMyTEF5c0VUVVVmWkE2MmpuNVNwdEEiLCJtYWMiOiI0MzVlOTRiZmYwYWE4MGRlMTMxOWExZDU0NGEzNDAyZWM5NGQ1MmQ3Y2ZkZTU5MzljMzg3M2ZkYjVkNzYwZGIyIiwidGFnIjoiIn0%3D; laravel_session=eyJpdiI6ImZYZjRYM1phdFdiazdpUnR5eFEvdUE9PSIsInZhbHVlIjoiWWorNnZlamRqNW4yNVJ3akdJZFF1dzRoVjBKbnJ5REYwRlFScGx2bFB1V3Z2aG5GMk9QelVQbkcxcVV3amYrTjNtdVJtNDJ5M1FYUXYzS21ZbEZPUnJLTER2L2J5T0NGeHV0RlJOU09pWUZmQVNlenZKcVRSKzJ2cWNueGR5cXQiLCJtYWMiOiJiOWQ3NmE2ZmM3ZTcwNGIzZjNkYTE1Y2EwYTliYWNlMWQ5Nzg0NjcxZTFlZDMwOTFlMmQ3NjI2YjY2ODhjYjlkIiwidGFnIjoiIn0%3D"
+    );
+
+    var requestOptions = {
+      method: "GET",
+      headers: myHeaders,
+      redirect: "follow",
+    };
+
+    fetch("http://127.0.0.1:8000/api/products", requestOptions)
+      .then((response) => response.json())
+      .then((result) => {
+        setChecked(false);
+        console.log(result);
+        setProducts(result)
+      })
+      .catch((error) => console.log("error", error));
+  };
+
+  useEffect(() => {
+    initProducts();
+  }, []);
+  const render = () => {
+    if (check)
+      return (
+        <div className="text-center space-y-3">
+          <p className="text-2xl font-semibold">Historical price</p>
+          <p className="text-2xl">Loading ...</p>
         </div>
-    )
-}
+      );
+    else {
+      // console.log(1);
+      const listItems = products.map((tasks) => (
+        <div class="col-4" style={{ padding: "20px" }}>
+          <div class="" className="size">
+            <img class="card-img-top" src={tasks.url} alt="Card img" />
+            <div class="card-body">
+              <h4 class="card-title">{tasks.productName}</h4>
+              <p class="card-text">Type : {tasks.productLine}</p>
+              <p class="card-text">Scale : {tasks.productScale}</p>
+              <p class="card-text">Vendor : {tasks.productVendor}</p>
+              <p class="card-text">Description : {tasks.productDescription}</p>
+              <p class="card-text">Stock : {tasks.quantityInStock}</p>
+              <p class="card-text">Price : {tasks.buyPrice}</p>
+              <p class="card-text">MSRP : {tasks.MSRP}</p>
+              <a href="" class="btn btn-primary">
+                Add to cart
+              </a>
+            </div>
+          </div>
+        </div>
+      ));
 
+      return <div class="row justify-content-start">{listItems}</div>;
+    }
+  };
+
+
+  return (
+    <div class="">
+      <h1 className="product">
+        Shop <span style={{ color: "white" }}>Today</span>
+      </h1>
+      <div class="container">{render()}</div>
+    </div>
+    
+  );
+};
 
 export default Shop;
