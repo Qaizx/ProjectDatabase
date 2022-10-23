249d8549127b004af10d1aea6c41722dadfb7d0e
Qaizx <kaothom@gmail.com>
2022-10-23 20:51:52 +0700

    edit info

--- front-end/src/Page/info/Info.js
+++ front-end/src/Page/info/Info.js
@@ -3,94 +3,150 @@ import Col from "react-bootstrap/Col";
 import Form from "react-bootstrap/Form";
 import Row from "react-bootstrap/Row";
 import "./Info.css";
+import { useState, useEffect } from "react";
 
 function Info() {
-  return (
-    <div className="pad">
-      <Form>
-        <Row className="mb-3">
-          <Form.Group as={Col} controlId="formGridEmail">
-            <Form.Label>First Name</Form.Label>
-            <Form.Control type="text" placeholder="Enter first name" />
-          </Form.Group>
+  const [profile, setProfile] = useState();
+  const [check, setChecked] = useState(true);
+  const CryptoJS = require("crypto-js");
 
-          <Form.Group as={Col} controlId="formGridPassword">
-            <Form.Label>Last Name</Form.Label>
-            <Form.Control type="text" placeholder="last name" />
-          </Form.Group>
-        </Row>
+  const getProfile = async () => {
+    const token = localStorage.getItem("token");
+    const username = CryptoJS.enc.Base64.parse(token).toString(
+      CryptoJS.enc.Utf8
+    );
 
-        <Row className="mb-3">
-          <Form.Group as={Col} controlId="formGridEmail">
-            <Form.Label>Contact First Name</Form.Label>
-            <Form.Control type="text" placeholder="Enter first name" />
-          </Form.Group>
+    // console.log(username);
 
-          <Form.Group as={Col} controlId="formGridPassword">
-            <Form.Label>Contact Last Name</Form.Label>
-            <Form.Control type="text" placeholder="last name" />
-          </Form.Group>
-        </Row>
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+    myHeaders.append(
+      "Cookie",
+      "XSRF-TOKEN=eyJpdiI6IlJJOFBKQ20xRjRUR0NhUCtTUEdyMHc9PSIsInZhbHVlIjoidnJySzRYMktmYVpUZkVzRDcrR0dQZjBBRDdkc0NpbmJ5cUdVdEc4T0p5Y0V5L1h3REVvUko2cWVvTUdaTFR4bm1lM3Arcmg1QklTTDBQMWM1RlRiVGdvN2l6RWt1MzNPUURnZ1hWSVhwL3VLYWlBTmFmc1ZYc1RkNFgrM2pBRUkiLCJtYWMiOiJjODQ2ZjBkNDI3ZTEwMTQyMjNiNmQ5NTJiZWIyYmI1NzViZTg0OWI1Y2E3OTdkNTE1NDIxNTY1ZDdhNWZjY2M5IiwidGFnIjoiIn0%3D; laravel_session=eyJpdiI6IjhHUUk5cW91blA0YkZQVTFiVjl2dFE9PSIsInZhbHVlIjoiTVJJa3lzUjk5V3kwdFRmVkNjMDN3QXAxQjhhM2JFN0FabDZpT2VFbWtaWTMrT0tIRUZqQ2I0VTB4VlpFYVNiMXZiZElUZWd6c0N1RHhFR0Y3YlBmNHVjQmN2TnM3eWRQNmJ6NjJSU1psdFFNVDZvcExwT2pwWjgvY2dxMnA0SjIiLCJtYWMiOiIzYjE5ZTZlOTU4OWI1ODE3ZmY2MDZkOGM0OTBjMGM2NTI5MmY4MDAyYzdhZmYwMjI3NGZmYTg0YjM4ODAyMWU0IiwidGFnIjoiIn0%3D"
+    );
 
-        <Form.Group className="mb-3" controlId="formGridAddress1">
-          <Form.Label>Phone</Form.Label>
-          <Form.Control placeholder="09x-xxx-xxxx" />
-        </Form.Group>
+    var raw = JSON.stringify({
+      username: username,
+    });
 
-        <Form.Group className="mb-3" controlId="formGridAddress1">
-          <Form.Label>Address</Form.Label>
-          <Form.Control placeholder="1234 Main St" />
-        </Form.Group>
+    var requestOptions = {
+      method: "POST",
+      headers: myHeaders,
+      body: raw,
+      redirect: "follow",
+    };
 
-        <Form.Group className="mb-3" controlId="formGridAddress2">
-          <Form.Label>Address 2</Form.Label>
-          <Form.Control placeholder="Apartment, studio, or floor" />
-        </Form.Group>
+    fetch("http://127.0.0.1:8000/api/getProfile", requestOptions)
+      .then((response) => response.json())
+      .then((result) => {
+        setProfile(result);
+        console.log(profile);
+        setChecked(false);
+      })
+      .catch((error) => console.log("error", error));
+  };
 
-        <Row className="mb-3">
-          <Form.Group as={Col} controlId="formGridCity">
-            <Form.Label>City</Form.Label>
-            <Form.Control />
-          </Form.Group>
+  useEffect(() => {
+    getProfile();
+  }, []);
 
-          <Form.Group as={Col} controlId="formGridState">
-            <Form.Label>State</Form.Label>
-            <Form.Control />
-            {/* <Form.Select defaultValue="Choose...">
-              <option>Choose...</option>
-              <option>...</option>
-            </Form.Select> */}
-          </Form.Group>
-
-          <Form.Group as={Col} controlId="formGridState">
-            <Form.Label>Country</Form.Label>
-            <Form.Control />
-            {/* <Form.Select defaultValue="Choose...">
-              <option>Choose...</option>
-              <option>...</option>
-            </Form.Select> */}
-          </Form.Group>
-
-          <Form.Group as={Col} controlId="formGridZip">
-            <Form.Label>Postal Code</Form.Label>
-            <Form.Control />
-          </Form.Group>
-        </Row>
-
-        <div style={{textAlign: "right"}}>
-          <Button
-            variant="danger"
-            type="submit"
-            style={{ margin: "10px  10px" }}
-            href="/profile"
-          >
-            Cancel
-          </Button>
-          <Button variant="primary" type="submit">
-            Submit
-          </Button>
+  const render = () => {
+    if (check) {
+      return (
+        <div style={{ textAlign: "center", margin: "100px 0px" }}>
+          <h1>Loading . . .</h1>
         </div>
-      </Form>
+      );
+    } else {
+      return (
+        <div className="pad">
+          <Form>
+            <Row className="mb-3">
+              <Form.Group as={Col} controlId="formGridEmail">
+                <Form.Label>First Name</Form.Label>
+                <Form.Control type="text" placeholder={profile.contactFirstName} />
+              </Form.Group>
+
+              <Form.Group as={Col} controlId="formGridPassword">
+                <Form.Label>Last Name</Form.Label>
+                <Form.Control type="text" placeholder={profile.contactLastName} />
+              </Form.Group>
+            </Row>
+
+            <Form.Group className="mb-3" controlId="formGridAddress1">
+              <Form.Label>Name</Form.Label>
+              <Form.Control placeholder={profile.customerName}/>
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formGridAddress1">
+              <Form.Label>Phone</Form.Label>
+              <Form.Control placeholder={profile.phone} />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formGridAddress1">
+              <Form.Label>Address</Form.Label>
+              <Form.Control placeholder={profile.addressLine1} />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formGridAddress2">
+              <Form.Label>Address 2</Form.Label>
+              <Form.Control placeholder={profile.addressLine2}/>
+            </Form.Group>
+
+            <Row className="mb-3">
+              <Form.Group as={Col} controlId="formGridCity">
+                <Form.Label>City</Form.Label>
+                <Form.Control placeholder={profile.city} />
+              </Form.Group>
+
+              <Form.Group as={Col} controlId="formGridState">
+                <Form.Label>State</Form.Label>
+                <Form.Control placeholder={profile.state} />
+                {/* <Form.Select defaultValue="Choose...">
+              <option>Choose...</option>
+              <option>...</option>
+            </Form.Select> */}
+              </Form.Group>
+
+              <Form.Group as={Col} controlId="formGridState">
+                <Form.Label>Country</Form.Label>
+                <Form.Control placeholder={profile.Country} />
+                {/* <Form.Select defaultValue="Choose...">
+              <option>Choose...</option>
+              <option>...</option>
+            </Form.Select> */}
+              </Form.Group>
+
+              <Form.Group as={Col} controlId="formGridZip">
+                <Form.Label>Postal Code</Form.Label>
+                <Form.Control placeholder={profile.postalCode}/>
+              </Form.Group>
+            </Row>
+
+            <div style={{ textAlign: "right" }}>
+              <Button
+                variant="danger"
+                type="submit"
+                style={{ margin: "10px  10px" }}
+                href="/profile"
+              >
+                Cancel
+              </Button>
+              <Button variant="primary" type="submit">
+                Submit
+              </Button>
+            </div>
+          </Form>
+        </div>
+      );
+    }
+  };
+  return (
+    <div>
+      <div>
+        <h1 className="headerText_info"> Edit Information</h1>
+      </div>
+      {render()}
     </div>
   );
 }
