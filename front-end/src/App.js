0176a9b19e06c84dc2f5f571b37cbd6dcc2ee8f5
DarkTXYZ <pawaretmontfort@gmail.com>
2022-10-29 14:20:31 +0700

    decorate Home page

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -15,8 +15,13 @@ import Profile from "./Page/profile/Profile";
 import Salesrep from "./Page/salesrep/Salesrep";
 import Money from "./Page/hackmoney/Money";
 import "@fontsource/jetbrains-mono";
+import { useCallback } from "react";
+import Particles from "react-tsparticles";
+import { loadFull } from "tsparticles";
+
 
 function App() {
+
   const [checkLogin, setLogin] = useState(false);
   const CryptoJS = require("crypto-js");
   const token = localStorage.getItem("token");
@@ -82,11 +87,90 @@ function App() {
     }
   };
 
+  const particlesInit = useCallback(async (engine) => {
+    console.log(engine);
+    // you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
+    // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+    // starting from v2 you can add only the features you need reducing the bundle size
+    await loadFull(engine);
+  }, []);
+
+  const particlesLoaded = useCallback(async (container) => {
+    await console.log(container);
+  }, []);
+
+  function topFunction() {
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  }
+
   return (
     <div>
-      <Routes>{ro()}</Routes>
+
+      <Routes id = "head" >{ro()}</Routes>
       {/* <Parent/> */}
+      <Particles
+        className="background"
+        id="tsparticles"
+        init={particlesInit}
+        loaded={particlesLoaded}
+        options={{
+          background: {
+            color: {
+              value: "#FAD6A5",
+            },
+          },
+          fpsLimit: 120,
+          particles: {
+            color: {
+              value: "#61764B",
+            },
+            links: {
+              color: "#61764B",
+              distance: 150,
+              enable: true,
+              opacity: 0.5,
+              width: 1,
+            },
+            collisions: {
+              enable: true,
+            },
+            move: {
+              directions: "none",
+              enable: true,
+              outModes: {
+                default: "bounce",
+              },
+              random: false,
+              speed: 2,
+              straight: true,
+            },
+            number: {
+              density: {
+                enable: true,
+                area: 800,
+              },
+              value: 40,
+            },
+            opacity: {
+              value: 0.5,
+            },
+            shape: {
+              type: "circle",
+            },
+            size: {
+              value: { min: 1, max: 5 },
+            },
+          },
+          detectRetina: true,
+        }}
+      />
+      <button class="myBtn">
+        <a onClick={topFunction} style={{ color: "white" }}>
+          Top
+        </a>
+      </button>
     </div>
+
   );
 }
 
--- front-end/src/Page/home/Home.js
+++ front-end/src/Page/home/Home.js
@@ -4,49 +4,46 @@ import ShopTable from "./component/ShopTable";
 const Home = () => {
   return (
     <>
-      <h1 class="welcomeText" id="head">
-        Welcome to 
-      </h1>
-      <h1 class="shopText" id="head">
-       shop
-      </h1>
-      <div style={{ textAlign: "center" }}>
-        <button class="get_start" type="submit">
-          <span>
-            <a href="/shop" style={{ color: "white" }}>
-              Get Started&nbsp;
-            </a>
-            <svg
-              stroke="currentColor"
-              fill="currentColor"
-              stroke-width="0"
-              viewBox="0 0 448 512"
-              class="react-icon"
-              height="1em"
-              width="1em"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path d="M190.5 66.9l22.2-22.2c9.4-9.4 24.6-9.4 33.9 0L441 239c9.4 9.4 9.4 24.6 0 33.9L246.6 467.3c-9.4 9.4-24.6 9.4-33.9 0l-22.2-22.2c-9.5-9.5-9.3-25 .4-34.3L311.4 296H24c-13.3 0-24-10.7-24-24v-32c0-13.3 10.7-24 24-24h287.4L190.9 101.2c-9.8-9.3-10-24.8-.4-34.3z"></path>
-            </svg>
-          </span>
-        </button>
+      <div className="d-flex flex-column" style={{marginLeft:"2%"}}>
+        <h1 class="welcomeText" id="head">
+          Every purchase <br></br> will be made <br></br>  with pleasure.
+        </h1>
+        <div>
+          <button class="button-59" type="submit" >
+            <span>
+              <a href="/shop" style={{ color: "black" }}>
+                Get Started{" "}
+              </a>
+              <svg
+                stroke="black"
+                fill="black"
+                stroke-width="0"
+                viewBox="0 0 448 512"
+                class="react-icon"
+                height="1em"
+                width="1em"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path d="M190.5 66.9l22.2-22.2c9.4-9.4 24.6-9.4 33.9 0L441 239c9.4 9.4 9.4 24.6 0 33.9L246.6 467.3c-9.4 9.4-24.6 9.4-33.9 0l-22.2-22.2c-9.5-9.5-9.3-25 .4-34.3L311.4 296H24c-13.3 0-24-10.7-24-24v-32c0-13.3 10.7-24 24-24h287.4L190.9 101.2c-9.8-9.3-10-24.8-.4-34.3z"></path>
+              </svg>
+            </span>
+          </button>
 
-        <button class="product_today">
-          <a href="#down" style={{ color: "white" }}>
-            Product Today
-          </a>
-        </button>
+          <button class="button-59">
+            <a href="#down" style={{ color: "black" }}>
+              Product Today
+            </a>
+          </button>
+        </div>
+
       </div>
+
 
       <div id="down">
-        <ShopTable/>
+        <ShopTable />
       </div>
 
-      <button class="myBtn">
-        <a href="#head" style={{ color: "white" }}>
-          Top
-        </a>
-      </button>
+
     </>
   );
 };
