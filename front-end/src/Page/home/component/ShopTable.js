7f1ab97bef638665e095499b4f28316fc770d603
Qaizx <kaothom@gmail.com>
2022-10-30 02:34:28 +0700

    add to cart

--- front-end/src/Page/home/component/ShopTable.js
+++ front-end/src/Page/home/component/ShopTable.js
@@ -4,12 +4,18 @@ import "./ShopTable.css";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 import { Link } from "react-router-dom";
+import Swal from "sweetalert2";
+import withReactContent from "sweetalert2-react-content";
 
 const ShopTable = () => {
   const [products, setProducts] = useState([]);
   const [check, setChecked] = useState(true);
+  const CryptoJS = require("crypto-js");
   const token = localStorage.getItem("token");
   const [disabled, setDisabled] = useState(true);
+  const username = CryptoJS.enc.Base64.parse(token).toString(CryptoJS.enc.Utf8);
+
+  const MySwal = withReactContent(Swal);
 
   const checkToken = () => {
     if (token == null) {
@@ -38,12 +44,38 @@ const ShopTable = () => {
     initProducts();
   }, []);
 
-  const handleClick = (IDProduct) => {
-    console.log(IDProduct);
-    localStorage.setItem("IDProduct", IDProduct);
-    window.location.href = "/product";
-  };
+  const handleClick = () => {
+    MySwal.fire({
+      title: <strong>Add Success</strong>,
+      icon: "success",
+    });
 
+    const IDProduct = localStorage.getItem("IDProduct");
+    var myHeaders = new Headers();
+    myHeaders.append("Accept", "application/json");
+    myHeaders.append("Content-Type", "application/json");
+    myHeaders.append(
+      "Cookie",
+      "XSRF-TOKEN=eyJpdiI6IldHQXF5UzNuM0NkQ0pZQ3huQ3hJSkE9PSIsInZhbHVlIjoiWU1aeVhBQUNZYmpLc1BITytPL0Z4VDhMTStiQUdBVzJ0d2ZoSGNCeU5RYmF3aXRDbUQzZDBaWHJTYklrQUI2NVBSR1BHd3BQTnNoTFdPSHR2Sll0dWFMTDFjd3JLMEpjeHNBZ3dKY1RBekFLSG5EWHRiQk1XNXJBc2lCTjhEMG4iLCJtYWMiOiI2ZjgwZGFhZWM1NTA0NzkzMDBkM2IwYmVjMDM2ZjAwNDUxNzNjOGIzNTU5NGYzMzEzODdlMzUyMTU4MzQxMjU5IiwidGFnIjoiIn0%3D; laravel_session=eyJpdiI6ImVLS25tSS95QS9BMmE5a0NmL0p6YWc9PSIsInZhbHVlIjoiWklzU05TWXlWVVc1ZEI5QVNwQUVWQUd4OHhJTkkwbTFqRVFrUnd1M0dYZExvd0N3b3JnNEo5aXVRQ2Fqc2F4L3Q2am5VWnhJRy9NV2VjL3ZNMFUyQzB4b3lQcTRxcTZIbFZMQ21ZaUh3NGlsVWdmaEtJTXFaVzJDUkpUYzBrMFkiLCJtYWMiOiIxOTdlODg2NTZmNTVjMDgzNTQwNTg3YmVlNGUxMTVkOTY3NjM2ZTFlZDJlOTA4ZWQxY2Y4MWNmMGIxYTA0NzAyIiwidGFnIjoiIn0%3D"
+    );
+
+    var raw = JSON.stringify({
+      username: username,
+      productCode: IDProduct,
+    });
+
+    var requestOptions = {
+      method: "POST",
+      headers: myHeaders,
+      body: raw,
+      redirect: "follow",
+    };
+
+    fetch("http://127.0.0.1:8000/api/addToCart", requestOptions)
+      .then((response) => response.json())
+      .then((result) => console.log(result))
+      .catch((error) => console.log("error", error));
+  };
   const handleMouseMove = (IDProduct) => {
     // console.log(IDProduct);
     localStorage.setItem("IDProduct", IDProduct);
