2f04284b654504e7bd976749091439d2bbb9b746
Qaizx <kaothom@gmail.com>
2022-10-30 00:36:53 +0700

    navbar that not lack

--- front-end/src/component/NavbarRegister.js
+++ front-end/src/component/NavbarRegister.js
@@ -6,26 +6,51 @@ const NavbarRegister = () => {
   return (
     <>
       <div class="navbar_info d-flex justify-content-between align-items-center">
-
         <div>
-          <a href="/" className="gradient">Home</a>
-          <a href="/shop" className="gradient">Shop</a>
+          <Link to="/" className="gradient">
+            Home
+          </Link>
+          <Link to="/shop" className="gradient">
+            Shop
+          </Link>
         </div>
 
         <div class="drop">
           <button class="gradient" style={{ height: "62px", width: "62px" }}>
-            <img src="https://cdn-icons-png.flaticon.com/512/7710/7710488.png" className="logo"></img>
+            <img
+              src="https://cdn-icons-png.flaticon.com/512/7710/7710488.png"
+              className="logo"
+            ></img>
           </button>
           <div className="drop-content">
             <div>
-              <a href="/login">Login <img align="right" style={{marginLeft:"10px" }} src="https://cdn-icons-png.flaticon.com/512/1828/1828391.png " alt="" width="30" height="30"></img></a>
+              <Link to="/login" className="gradient" style={{backgroundColor: "white"}}>
+                Login{" "}
+                <img
+                  align="right"
+                  style={{ marginLeft: "10px" }}
+                  src="https://cdn-icons-png.flaticon.com/512/1828/1828391.png "
+                  alt=""
+                  width="30"
+                  height="30"
+                ></img>
+              </Link>
             </div>
             <div>
-              <a href="/register">Register <img align="right" style={{ marginLeft:"10px" }} src="https://cdn-icons-png.flaticon.com/512/2910/2910768.png " alt="" width="30" height="30"></img></a>
+              <Link to="/register" className="gradient" style={{backgroundColor: "white"}}>
+                Register{" "}
+                <img
+                  align="right"
+                  style={{ marginLeft: "10px" }}
+                  src="https://cdn-icons-png.flaticon.com/512/2910/2910768.png "
+                  alt=""
+                  width="30"
+                  height="30"
+                ></img>
+              </Link>
             </div>
           </div>
         </div>
-
       </div>
       <Outlet />
     </>
