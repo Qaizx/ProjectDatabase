79a7d23366d1146576d42e83d1d6bc1b0d22de3c
Qaizx <kaothom@gmail.com>
2022-10-29 14:30:46 +0700

    add to cart

--- front-end/src/Page/product/Product.js
+++ front-end/src/Page/product/Product.js
@@ -2,12 +2,18 @@ import "./Product.css";
 import { Button, Row, Form, Col, Container } from "react-bootstrap";
 import React from "react";
 import { useState, useEffect } from "react";
+import Swal from "sweetalert2";
+import withReactContent from "sweetalert2-react-content";
 
 const Product = () => {
   const [product, setProduct] = useState();
   const [check, setChecked] = useState(true);
   const token = localStorage.getItem("token");
   const [disabled, setDisabled] = useState(true);
+  const CryptoJS = require("crypto-js");
+  const username = CryptoJS.enc.Base64.parse(token).toString(CryptoJS.enc.Utf8);
+
+  const MySwal = withReactContent(Swal);
 
   const checkToken = () => {
     if (token == null) {
@@ -15,6 +21,45 @@ const Product = () => {
     } else {
       setDisabled(false);
     }
+  };
+
+  const typeFilter = (type) => {
+    console.log(type);
+    localStorage.setItem("type", type);
+    window.location.href = "/shop";
+  };
+
+  const handleClick = () => {
+    MySwal.fire({
+      title: <strong>Add Success</strong>,
+      icon: "success",
+    });
+
+    const IDProduct = localStorage.getItem("IDProduct");
+    var myHeaders = new Headers();
+    myHeaders.append("Accept", "application/json");
+    myHeaders.append("Content-Type", "application/json");
+    myHeaders.append(
+      "Cookie",
+      "XSRF-TOKEN=eyJpdiI6IldHQXF5UzNuM0NkQ0pZQ3huQ3hJSkE9PSIsInZhbHVlIjoiWU1aeVhBQUNZYmpLc1BITytPL0Z4VDhMTStiQUdBVzJ0d2ZoSGNCeU5RYmF3aXRDbUQzZDBaWHJTYklrQUI2NVBSR1BHd3BQTnNoTFdPSHR2Sll0dWFMTDFjd3JLMEpjeHNBZ3dKY1RBekFLSG5EWHRiQk1XNXJBc2lCTjhEMG4iLCJtYWMiOiI2ZjgwZGFhZWM1NTA0NzkzMDBkM2IwYmVjMDM2ZjAwNDUxNzNjOGIzNTU5NGYzMzEzODdlMzUyMTU4MzQxMjU5IiwidGFnIjoiIn0%3D; laravel_session=eyJpdiI6ImVLS25tSS95QS9BMmE5a0NmL0p6YWc9PSIsInZhbHVlIjoiWklzU05TWXlWVVc1ZEI5QVNwQUVWQUd4OHhJTkkwbTFqRVFrUnd1M0dYZExvd0N3b3JnNEo5aXVRQ2Fqc2F4L3Q2am5VWnhJRy9NV2VjL3ZNMFUyQzB4b3lQcTRxcTZIbFZMQ21ZaUh3NGlsVWdmaEtJTXFaVzJDUkpUYzBrMFkiLCJtYWMiOiIxOTdlODg2NTZmNTVjMDgzNTQwNTg3YmVlNGUxMTVkOTY3NjM2ZTFlZDJlOTA4ZWQxY2Y4MWNmMGIxYTA0NzAyIiwidGFnIjoiIn0%3D"
+    );
+
+    var raw = JSON.stringify({
+      username: username,
+      productCode: IDProduct,
+    });
+
+    var requestOptions = {
+      method: "POST",
+      headers: myHeaders,
+      body: raw,
+      redirect: "follow",
+    };
+
+    fetch("http://127.0.0.1:8000/api/addToCart", requestOptions)
+      .then((response) => response.json())
+      .then((result) => console.log(result))
+      .catch((error) => console.log("error", error));
   };
 
   const initProducts = async () => {
@@ -99,7 +144,9 @@ const Product = () => {
                           <p class="card-text">
                             <b>MSRP :</b> {product.MSRP}
                           </p>
-                          <button disabled={disabled} href="/cart">Add product</button>
+                          <button disabled={disabled} onClick={handleClick}>
+                            Add product
+                          </button>
                         </div>
                       </div>
                     </div>
