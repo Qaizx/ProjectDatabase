b671a393562104c39f4b2088f8f934c869b33d28
Qaizx <kaothom@gmail.com>
2022-11-06 17:46:46 +0700

    button css

--- front-end/src/Page/history/History.js
+++ front-end/src/Page/history/History.js
@@ -96,7 +96,7 @@ const History = () => {
                     <div class="col-sm-1 d-flex justify-content-center">
                         <Link to="/profile">
                             <button
-                                class="add-experience"
+                                class="buttonn button4"
                                 type="button"
                             >
                                 Back
