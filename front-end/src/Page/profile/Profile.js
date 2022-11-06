ac9e09729e63b708f0d2f9614b7e2f2b85075967
Qaizx <kaothom@gmail.com>
2022-11-06 20:24:56 +0700

    country

--- front-end/src/Page/profile/Profile.js
+++ front-end/src/Page/profile/Profile.js
@@ -160,7 +160,7 @@ const Profile = () => {
                     </div>
                     <div class="col-md-12">
                       <label class="labels">Country</label>
-                      <h5>{profile.country}</h5>
+                      <h5>{profile.country ? profile.country : '-'}</h5>
                     </div>
                     <div class="col-md-12">
                       <label class="labels">Postal Code</label>
